9aa8f4cea24cc765fe6e60e76a431a72861bc29c
a-pilipchuk <hanna.pilipchuk@gmail.com>
2020-04-17 10:34:07 +0300

    Added an assertion for NoSuchElementException

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,22 @@
 from selenium.webdriver import Remote as RemoteWebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     # добавляем конструктор — метод, который вызывается, когда мы создаем объект.
     # В него в качестве параметров мы передаем экземпляр драйвера и url адрес.
     # Внутри конструктора сохраняем эти данные как аттрибуты нашего класса.
-    def __init__(self, browser: RemoteWebDriver, url):
+    def __init__(self, browser: RemoteWebDriver, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
 # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url:
@@ -9,3 +10,11 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
 
 # pytest -v --tb=line --language=en test_main_page.py
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
