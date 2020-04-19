796abf10e8c845e21af393927579a1e4878af219
a-pilipchuk <hanna.pilipchuk@gmail.com>
2020-04-19 21:36:14 +0300

    step 4-3-8, move login link methods to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,8 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from .locators import BasePageLocators
+
 
 class BasePage():
     # добавляем конструктор — метод, который вызывается, когда мы создаем объект.
@@ -42,6 +44,13 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
